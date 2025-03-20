1f363f18a4da30bf9e068f999f5dcf25a58f2607
Luis Mera <luismera86@gmail.com>
2025-03-19 23:04:02 -0300

    add typeorm

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import envConfig from "./config/config";
+import { AppDataSource } from "./config/datasource.configi";
 
 const { PORT } = envConfig;
 
@@ -8,6 +9,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => console.log(error));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/config/datasource.configi.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "test",
+  password: "test",
+  database: "test",
+  synchronize: true,
+  logging: true,
+  entities: [],
+  subscribers: [],
+  migrations: [],
+})
+
+AppDataSource.initialize()
+    .then(() => {
+        // here you can start to work with your database
+    })
+    .catch((error) => console.log(error))
