f8ee5785408acd8bccf8db3af9290222a1c5b447
Luis Mera <luismera86@gmail.com>
2025-04-30 00:23:38 -0300

    Agregar dependencias de CORS y tipos de TypeScript en package.json. Refactorizar app.ts para utilizar la nueva clase Server y simplificar la inicialización de la aplicación.

---
+++ src/config/server.config.ts
@@ -0,0 +1,55 @@
+import express from "express";
+import { BaseRouter } from "../common/router/router";
+import { customExceptions } from "../exceptions/custom-exceptions";
+import { NotFoundException } from "../exceptions/exceptions";
+import AppDataSource from "./datasource.config";
+import { LoggerService } from "../common/utils/logger";
+import envConfig from "./env.config";
+import swaggerUi from "swagger-ui-express";
+import { openApiDoc } from "../docs/swagger";
+import cors from "cors";
+export class Server {
+  private app: express.Application;
+  private logger: LoggerService;
+
+  constructor() {
+    this.app = express();
+    this.logger = new LoggerService("Server");
+  }
+
+  public start() {
+    this.middleware();
+    this.router();
+    this.database();
+    this.listen();
+  }
+
+  private middleware() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cors());
+  }
+
+  private router() {
+    this.app.use("/api", BaseRouter.router);
+    this.app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiDoc));
+    this.app.use("*", (req, res, next) => {
+      next(new NotFoundException("Path not found"));
+    });
+    this.app.use(customExceptions);
+  }
+
+  private database() {
+    AppDataSource.initialize()
+      .then(() => {
+        this.logger.info("Database connected");
+      })
+      .catch((error) => this.logger.error(error));
+  }
+
+  private listen() {
+    this.app.listen(envConfig.DB_PORT, () => {
+      this.logger.info(`Server is running on port ${envConfig.DB_PORT}`);
+    });
+  }
+}
