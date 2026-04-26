8b3455575a6ed950620446a5fffecb6af9f88c89
Luis Mera <luismera86@gmail.com>
2026-04-26 00:20:23 -0300

    Actualiza dependencias y reemplaza bcrypt con argon2 para el hash de contraseñas.

--- src/common/utils/hash.util.ts
+++ src/common/utils/hash.util.ts
@@ -1,10 +1,9 @@
-import bcrypt from "bcrypt";
+import * as argon2 from "argon2";
 
-export const hashPassword = (value: string) => {
-    const salt = bcrypt.genSaltSync(10);
-    return bcrypt.hashSync(value, salt);
+export const hashPassword = (value: string): Promise<string> => {
+    return argon2.hash(value, { type: argon2.argon2id });
 };
 
-export const compareHash = (value: string, hashedValue: string) => {
-    return bcrypt.compareSync(value, hashedValue);
+export const compareHash = (value: string, hashedValue: string): Promise<boolean> => {
+    return argon2.verify(hashedValue, value);
 };
